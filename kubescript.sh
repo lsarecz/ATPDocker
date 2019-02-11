03b7d50289caa2e3ccf2658de216dd466c6f8ad1
Jan Leemans <janleemans@users.noreply.github.com>
2019-02-11 20:45:04 +0100

    Update kubescript.sh

--- kubescript.sh
+++ kubescript.sh
@@ -1,11 +1,4 @@
-mkdir -p $HOME/.kube
-
-# oci ce cluster create-kubeconfig --cluster-id ocid1.cluster.oc1.eu-frankfurt-1.aaaaaaaaaezgizldmjrdcyrtgqydszlbgqygenbxmm3ggntfgcrgemzzmjrw --file $HOME/.kube/config --region eu-frankfurt-1
-# export KUBECONFIG=$HOME/.kube/config
-
 export KUBECONFIG=./mykubeconfig
-mkdir ctd
-cp $KUBECONFIG ctd/kubeconfig
 kubectl config view
 kubectl get nodes
 kubectl delete deployment atp2 --ignore-not-found=true
